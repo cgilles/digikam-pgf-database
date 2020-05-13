c775aa63da58c1b9c7a4bab23246e1a060d449f8
Gilles Caulier <caulier.gilles@gmail.com>
2020-05-13 15:13:41 +0200

    init and add relevant code to uncompress database thumbnail blob and save as PNG

---
+++ loadpgfblob.cpp
@@ -0,0 +1,78 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2009-02-04
+ * Description : a command line tool to load PGF data blob from digiKam
+ *               database and convert to PNG using QImage
+ *
+ * Copyright (C) 2011-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
+
+#include <QFile>
+#include <QIODevice>
+#include <QImage>
+#include <QDebug>
+#include <QByteArray>
+
+// Local includes
+
+#include "pgfutils.h"
+
+using namespace Digikam;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        qDebug() << "loadpgfdata - Load PGF blob data and save to PNG";
+        qDebug() << "Usage: <pgf_blob_file>";
+        return -1;
+    }
+
+    qDebug() << "Using LibPGF version: " << PGFUtils::libPGFVersion();
+    QImage img;
+
+    // Write PGF file.
+
+    QString fname(QString::fromUtf8(argv[1]));
+    QFile   file(fname);
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Cannot open PGF file to read...";
+        return -1;
+    }
+
+    QByteArray blob(file.size(), '\x00');
+    QDataStream stream(&file);
+    stream.readRawData(blob.data(), blob.size());
+
+    QByteArray data = qUncompress(blob);
+
+    // PGF => QImage conversion
+
+    if (!PGFUtils::readPGFImageData(data, img))
+    {
+        qDebug() << "loadPGFScaled failed...";
+        return -1;
+    }
+
+    img.save(file.fileName() + QString::fromUtf8("-converted.png"), "PNG");
+
+    return 0;
+}
